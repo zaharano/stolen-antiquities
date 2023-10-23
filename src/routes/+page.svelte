b472e42c711730c90ead9c3efe380972f458ba89
Zach <zraisnorman@gmail.com>
2023-10-23 13:03:37 -0400

    remove unused stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,14 @@
 <script>
-	import Map from './Map.svelte';
-	import Game from './components/Game.svelte';
-
 	/** @type {import('./$types').PageData} */
-	export let data;
 </script>
 
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
-	<link rel="stylesheet" href="/path/to/leaflet.css" />
 </svelte:head>
 
-<Map />
-<Game object={data.object} />
+<!-- <Map /> -->
+<div>Museum Description</div>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
 </style>
