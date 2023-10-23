e462b4c4d2fc52c8cec94d69d7d87ff68934651f
Zach <zraisnorman@gmail.com>
2023-10-23 13:04:22 -0400

    loader from fullData local file w type

---
+++ src/routes/timeline/+page.server.ts
@@ -0,0 +1,47 @@
+import type { MuseumObject } from './timelineGame.js';
+import type { PageServerLoad } from './$types.js';
+
+export const load: PageServerLoad = async ({ fetch }) => {
+	// Load the tenRandom
+	// const object = await fetchElementWithField(randomHighlightURL(), 'primaryImage', fetch);
+	const localSet = await fetch('/fullData.json');
+
+	const objects = (await localSet.json()) as MuseumObject[];
+
+	return { objects };
+};
+
+// async function fetchElementWithField(
+// 	apiEndpoint: string,
+// 	desiredField: string,
+// 	customFetch: typeof fetch
+// ) {
+// 	while (true) {
+// 		try {
+// 			const response = await customFetch(apiEndpoint);
+// 			if (!response.ok) {
+// 				throw new Error(`Failed to fetch data from the API: ${response.statusText}`);
+// 			}
+// 			const data = await response.json();
+
+// 			if (data[desiredField]) {
+// 				return data;
+// 			}
+// 		} catch (error) {
+// 			console.error(error);
+// 		}
+// 	}
+// }
+
+// const objects = await localSet.json();
+// const idArray = objects.map((object) => object.objectID);
+
+// const responses = await Promise.all(idArray.map((id) => fetch(buildRequestURL(id))));
+
+// const data = responses.map(async (response) => {
+// 	if (!response.ok) {
+// 		throw new Error(`Failed to fetch data from the API: ${response.statusText}`);
+// 	} else {
+// 		return await response.json();
+// 	}
+// });
